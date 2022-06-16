eeb266f8d8b7159fbd9b58f91af8e5185241fa39
napthedev <phongna.dev@gmail.com>
2022-06-16 13:23:31 +0700

    Feat: add clip to clipboard button

--- client/src/pages/post/[slug].tsx
+++ client/src/pages/post/[slug].tsx
@@ -1,5 +1,9 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { formatDate, markdownToHTML } from "../../shared/utils";
+import {
+  copyToClipboard,
+  formatDate,
+  markdownToHTML,
+} from "../../shared/utils";
 import { useEffect, useRef } from "react";
 
 // @ts-ignore
@@ -25,6 +29,20 @@ const Post: NextPage<PostProps> = ({ data }) => {
   useEffect(() => {
     const anchors = new AnchorJS();
     anchors.add();
+
+    const pres = document.querySelectorAll("pre");
+    pres.forEach((pre) => {
+      pre.style.position = "relative";
+      const codeEl = pre.querySelector("code");
+      const button = document.createElement("button");
+      button.classList.add("copy-btn");
+      button.title = "Copy to clipboard";
+      button.innerHTML = `<svg style="height: 20px; width: 20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#fff" d="M320 64h-49.61C262.1 27.48 230.7 0 192 0S121 27.48 113.6 64H64C28.65 64 0 92.66 0 128v320c0 35.34 28.65 64 64 64h256c35.35 0 64-28.66 64-64V128C384 92.66 355.3 64 320 64zM192 48c13.23 0 24 10.77 24 24S205.2 96 192 96S168 85.23 168 72S178.8 48 192 48zM336 448c0 8.82-7.178 16-16 16H64c-8.822 0-16-7.18-16-16V128c0-8.82 7.178-16 16-16h18.26C80.93 117.1 80 122.4 80 128v16C80 152.8 87.16 160 96 160h192c8.836 0 16-7.164 16-16V128c0-5.559-.9316-10.86-2.264-16H320c8.822 0 16 7.18 16 16V448z"/></svg>`;
+
+      button.onclick = () => copyToClipboard(codeEl?.innerText as string);
+
+      pre.appendChild(button);
+    });
 
     commentContainerRef.current?.innerHTML &&
       (commentContainerRef.current.innerHTML = "");
@@ -59,7 +77,7 @@ const Post: NextPage<PostProps> = ({ data }) => {
         <SocialShare title={data.title} />
         <p className="text-lg">{data.description}</p>
         <article
-          className="prose prose-lg dark:prose-invert prose-headings:text-sky-600 dark:prose-headings:text-sky-400 prose-a:text-blue-600 dark:prose-a:text-blue-400 prose-headings:mt-8 prose-headings:mb-6 prose-pre:bg-[#121213]"
+          className="prose prose-lg dark:prose-invert prose-headings:text-sky-600 dark:prose-headings:text-sky-400 prose-a:text-blue-600 dark:prose-a:text-blue-400 prose-headings:mt-8 prose-headings:mb-6 prose-pre:bg-[#121213] prose-pre:p-4"
           dangerouslySetInnerHTML={{ __html: data.body }}
         ></article>
         <h1 className="mt-8 text-3xl">Bình luận</h1>
--- client/src/shared/utils.ts
+++ client/src/shared/utils.ts
@@ -9,6 +9,29 @@ export const formatDate = (date: string | number) => {
   return dayjs(date).format("MMMM DD, YYYY");
 };
 
+export const copyToClipboard = (text: string) => {
+  try {
+    if (navigator.clipboard && window.isSecureContext) {
+      return navigator.clipboard.writeText(text);
+    } else {
+      let textArea = document.createElement("textarea");
+      textArea.value = text;
+      textArea.style.position = "fixed";
+      textArea.style.left = "-999999px";
+      textArea.style.top = "-999999px";
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      return new Promise<void>((res, rej) => {
+        document.execCommand("copy") ? res() : rej();
+        textArea.remove();
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const markdownToHTML = (text: string) => {
   const md = MarkdownIt({
     xhtmlOut: true,
