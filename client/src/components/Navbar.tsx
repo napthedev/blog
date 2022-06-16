e0459fc1be07d88c240f23ca9c048c06d1154f48
napthedev <phongna.dev@gmail.com>
2022-06-16 16:14:09 +0700

    Feat: remove search, add darkmode using next-themes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,100 +1,45 @@
-import { FC, FormEvent, useEffect, useState } from "react";
-
+import { FC } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
 
 const Navbar: FC = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-
-  const router = useRouter();
-
-  const handleFormSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
-    if (inputValue.trim() && !loading) {
-      setLoading(true);
-      setInputValue("");
-      router.push(`search?q=${encodeURIComponent(inputValue.trim())}`);
-    }
-  };
-
-  useEffect(() => {
-    setLoading(false);
-    setIsActive(false);
-  }, [router.pathname, router.query?.q]);
+  const { setTheme, resolvedTheme } = useTheme();
 
   return (
-    <div
-      className={`${
-        isActive ? "h-[108px]" : "h-14"
-      } sm:!h-14 shadow dark:shadow-gray-600 flex flex-col sm:flex-row justify-center gap-4 sm:justify-between items-stretch sm:items-center px-6`}
-    >
-      <div className="flex items-center justify-between">
-        <Link href="/">
-          <a className="flex items-center gap-2">
-            <img className="h-8 w-8" src="/logo.png" alt="" />
-            <h1 className="text-lg">NAPTheDev{"'"}s Blog</h1>
-          </a>
-        </Link>
-        <button
-          onClick={() => setIsActive(!isActive)}
-          className="sm:hidden block"
-        >
-          {isActive ? (
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path
-                className="fill-white"
-                d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
-              />
-            </svg>
-          ) : (
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                className="fill-white"
-                d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"
-              />
-            </svg>
-          )}
+    <div className="h-14 shadow dark:shadow-gray-600 flex justify-between items-center px-6">
+      <Link href="/">
+        <a className="flex items-center gap-2">
+          <img className="h-8 w-8" src="/logo.png" alt="" />
+          <h1 className="text-lg">NAPTheDev{"'"}s Blog</h1>
+        </a>
+      </Link>
+      {resolvedTheme === "light" ? (
+        <button onClick={() => setTheme("dark")}>
+          <svg
+            className="w-6 h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="#000"
+              d="M32 256c0-123.8 100.3-224 223.8-224c11.36 0 29.7 1.668 40.9 3.746c9.616 1.777 11.75 14.63 3.279 19.44C245 86.5 211.2 144.6 211.2 207.8c0 109.7 99.71 193 208.3 172.3c9.561-1.805 16.28 9.324 10.11 16.95C387.9 448.6 324.8 480 255.8 480C132.1 480 32 379.6 32 256z"
+            />
+          </svg>
         </button>
-      </div>
-      <form
-        onSubmit={handleFormSubmit}
-        className={`relative ${isActive ? "flex" : "hidden"} sm:!flex`}
-      >
-        <input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          type="text"
-          className="outline-none bg-[#F0F2F5] dark:bg-[#3A3B3C] py-[6px] px-3 rounded-full placeholder:text-gray-400 w-full sm:w-auto"
-          placeholder="Tìm kiếm bài viết"
-        />
-        {loading ? (
-          <div className="w-5 h-5 border-2 border-gray-500 border-t-transparent rounded-full animate-spin absolute top-2 right-2"></div>
-        ) : (
-          <button className="absolute top-1/2 -translate-y-1/2 right-2">
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                className="fill-gray-500 dark:fill-gray-400"
-                d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"
-              />
-            </svg>
-          </button>
-        )}
-      </form>
+      ) : (
+        <button onClick={() => setTheme("light")}>
+          <svg
+            className="w-6 h-6"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#fff"
+              d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2H2zm17 0h3v2h-3zM5.637 19.778l-1.414-1.414 2.121-2.121 1.414 1.414zM16.242 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.344 7.759 4.223 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"
+            ></path>
+          </svg>
+        </button>
+      )}
     </div>
   );
 };
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -6,35 +6,38 @@ import Footer from "../components/Footer";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Script from "next/script";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta
-          property="fb:app_id"
-          content={process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}
-        />
-        <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
-      </Head>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-      <div id="fb-root"></div>
-      <Script
-        strategy="lazyOnload"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      ></Script>
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
+    <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+      <>
+        <Head>
+          <meta
+            property="fb:app_id"
+            content={process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}
+          />
+          <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
+        </Head>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+        <div id="fb-root"></div>
+        <Script
+          strategy="lazyOnload"
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></Script>
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
-      </Script>
-    </>
+        </Script>
+      </>
+    </ThemeProvider>
   );
 }
 
